d4861c711df93de81075e1e9c2ed3662205b4a96
Moutasem <moutasem.m.abdullatif@gmail.com>
2024-03-31 17:50:00 +0300

    implemented filtering and added partial template

--- dir/views.py
+++ dir/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 def home(request):
-    employees = Employee.objects.all()
 
-    return render(request, 'dir/employee.html', {'employees': employees})
+    q = request.GET.get('q','')
+    employees = Employee.objects.filter(first_name__icontains=q)
+    
+    if request.headers.get('hx-Request') != None:
+        template_name = 'dir/partial.html'
+    else:
+        template_name = 'dir/employee.html'
+
+    context = {'employees': employees}
+    return render(request, template_name, context)
